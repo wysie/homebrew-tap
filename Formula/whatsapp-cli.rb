2d3ee612a3450dab2ae1dffb20d951388acfb7ae
Wysie <wysie@users.noreply.github.com>
2026-05-07 11:32:05 +0800

    whatsapp-cli 0.2.1-wysie.4

--- Formula/whatsapp-cli.rb
+++ Formula/whatsapp-cli.rb
@@ -1,9 +1,9 @@
 class WhatsappCli < Formula
   desc "WhatsApp from your terminal"
   homepage "https://github.com/Wysie/whatsapp-cli"
-  url "https://github.com/wysie/whatsapp-cli/archive/refs/tags/v0.2.1-wysie.3.tar.gz"
-  sha256 "98b93fbd97ee6f8f45e5be6e802203f6194a3531b757a8888e34539a6895a004"
-  version "0.2.1-wysie.3"
+  url "https://github.com/wysie/whatsapp-cli/archive/refs/tags/v0.2.1-wysie.4.tar.gz"
+  sha256 "3e2782303b1a593416ec7b4d411c586816f8a65cb6a70cc8e109168df748ca59"
+  version "0.2.1-wysie.4"
   license "MIT"
 
   depends_on "go" => :build
