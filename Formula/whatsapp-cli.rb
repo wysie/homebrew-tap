546b4d28c2e1f8d60b0eed8465ab153d089528ce
Wysie <wysie@users.noreply.github.com>
2026-05-07 11:12:14 +0800

    whatsapp-cli 0.2.1-wysie.3

--- Formula/whatsapp-cli.rb
+++ Formula/whatsapp-cli.rb
@@ -1,16 +1,16 @@
 class WhatsappCli < Formula
   desc "WhatsApp from your terminal"
   homepage "https://github.com/Wysie/whatsapp-cli"
-  url "https://github.com/wysie/whatsapp-cli/archive/refs/tags/v0.2.1-wysie.2.tar.gz"
-  sha256 "bae968d1aeaa878238efb5f9820cc0a8a795e70a76400b82075661c902f43d6a"
-  version "0.2.1-wysie.2"
+  url "https://github.com/wysie/whatsapp-cli/archive/refs/tags/v0.2.1-wysie.3.tar.gz"
+  sha256 "98b93fbd97ee6f8f45e5be6e802203f6194a3531b757a8888e34539a6895a004"
+  version "0.2.1-wysie.3"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
     ENV["CGO_ENABLED"] = "1"
-    system "go", "build", "-tags", "sqlite_fts5", "-o", bin/"whatsapp", "./cmd/whatsapp"
+    system "go", "build", "-tags", "sqlite_fts5", "-ldflags", "-X github.com/eddmann/whatsapp-cli/internal/cli.version=#{version}", "-o", bin/"whatsapp", "./cmd/whatsapp"
   end
 
   test do
